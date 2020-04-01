78e4ea31e18ee997c2d824b1ccc6ec7b27327df8
Trevor Wang <Trevor.Wang@qq.com>
2020-04-01 15:47:57 +0800

    寻找两个有序数组的中位数

--- 4.寻找两个有序数组的中位数.java
+++ 4.寻找两个有序数组的中位数.java
@@ -40,8 +40,48 @@
 // @lc code=start
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1); // 保证 m <= n
+        }
+        int imin = 0;
+        int imax = m;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = (m + n + 1) / 2 - i; // 保证奇数时，中位数在左边
 
+            if (j != 0 && i != m && nums2[j - 1] > nums1[i]) {
+                imin = i + 1;
+            } else if (i != 0 && j != n && nums1[i - 1] > nums2[j]) {
+                imax = i - 1;
+            } else {
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums2[j - 1], nums1[i - 1]);
+                }
+                if ((m + n) % 2 == 1)
+                    return maxLeft;
+
+                int minRight = 0;
+
+                if (j == n) {
+                    minRight = nums1[i];
+                } else if (i == m) {
+                    minRight = nums2[j];
+                } else {
+                    minRight = Math.min(nums2[j], nums1[i]);
+                }
+                return (minRight + maxLeft) / 2.0;
+            }
+
+        }
+
+        return 0.0;
     }
 }
 // @lc code=end
-
