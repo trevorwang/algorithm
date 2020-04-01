6cad3ebb7889f7393054afe5e904a171c0e42fd4
Trevor Wang <trevor.wang@qq.com>
2020-04-01 22:56:41 +0800

    合并两个有序链表

---
+++ 21.合并两个有序链表.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        // recursion
+        // if (l1.val < l2.val) {
+        // l1.next = mergeTwoLists(l1.next, l2);
+        // return l1;
+        // } else {
+        // l2.next = mergeTwoLists(l1, l2.next);
+        // return l2;
+        // }
+
+        ListNode root = new ListNode(-1);
+        ListNode p = root;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        p.next = l1 == null ? l2 : l1;
+        return root.next;
+    }
+}
+// @lc code=end
