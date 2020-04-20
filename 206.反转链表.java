bd2ad44f23ea43316ec4013666251690b8c801d6
Trevor Wang <Trevor.Wang@qq.com>
2020-04-20 18:24:08 +0800

    206.反转链表.java 	23.合并k个排序链表.java

---
+++ 206.反转链表.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head, prev = null;
+
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
+// @lc code=end
---
+++ 23.合并k个排序链表.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ *
+ * [23] 合并K个排序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        ListNode newList = new ListNode(0);
+
+        return newList.next;
+    }
+}
+// @lc code=end
