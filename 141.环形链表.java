bb8014363dbf4ecca049d0f53ead142c9064244e
Trevor Wang <Trevor.Wang@qq.com>
2020-04-26 17:55:04 +0800

    141.环形链表

---
+++ 141.环形链表.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
+// @lc code=end
---
+++ 142.环形链表-ii.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=java
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                fast = head;
+                while (fast != null && slow != null) {
+                    if (fast == slow)
+                        return slow;
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+            }
+        }
+        return null;
+    }
+}
+// @lc code=end
